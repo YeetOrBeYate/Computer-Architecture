05b8cdd0129b2015570707b306dbee22f0e15360
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-21 13:38:21 -0400

    lecture opened my eyes on a lot of things

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,10 +1,6 @@
 """CPU functionality."""
-import sys
 
-LDI = 0b10000010
-PRN = 0b01000111
-HLT = 0b00000001
-MUL = 0b10100010
+import sys
 
 
 class CPU:
@@ -15,49 +11,77 @@ class CPU:
         self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
-        self.instruction = {LDI: self.ldi, PRN: self.prn, MUL: self.mul, HLT: self.hlt}
+        self.SP = 7
+        self.reg[self.SP] = 0xF4
+        self.instruction = {
+            0b00000001: self.HLT,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b10100010: self.MUL,
+            0b01000101: self.push,
+            0b01000110: self.pop,
+            0b10100000: self.add,
+            0b01010000: self.call,
+            0b00010001: self.ret
+        }
+
+    def HLT(self, op1, op2):
+        return (0, False)
+
+    def LDI(self, op1, op2):
+        self.reg[op1] = op2
+        return (3, True)
+
+    def PRN(self, op1, op2):
+        print(self.reg[op1])
+        return (2, True)
+
+    def MUL(self, op1, op2):
+        self.alu("MUL", op1, op2)
+        return (3, True)
+
+    def add(self, op1, op2):
+        self.alu('ADD', op1, op2)
+        return (3, True)
+
+    def call(self, op1, op2):
+        self.SP -= 1
+        self.ram[self.SP] = self.pc + 2
+        self.pc = self.reg[op1]
+        return (0, True)
+
+    def ret(self, op1, op2):
+        self.pc = self.ram[self.SP]
+        return (0, True)
 
     def load(self):
         """Load a program into memory."""
-        program = []
-        try:
-            with open(sys.argv[1]) as f:
-                for line in f:
-                    string_val = line.split('#')[0].strip()
-                    if string_val == '':
-                        continue
-                    v = int(string_val, 2)
-                    program = program + [v]
-        except FileNotFoundError:
-            print('File Not Found')
-            exit(1)
-        except IndexError:
-            print('You need to specify the file to run.')
-            exit(1)
 
         address = 0
+        program_filename = sys.argv[1]
 
         # For now, we've just hardcoded a program:
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(program_filename) as f:
+            for line in f:
+                line = line.split("#")
+                line = line[0].strip()
 
+                if line == "":
+                    continue
+
+                self.ram[address] = int(line, 2)
+                address += 1
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        #elif op == "SUB": etc
         elif op == "MUL":
-            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
-        # elif op == "SUB": etc
+            self.reg[reg_a] = (self.reg[reg_a] * self.reg[reg_b])
         else:
             raise Exception("Unsupported ALU operation")
-
-    def get_arg_count(self, value, comparision=0b11000000):
-        x = value & comparision
-        x = x >> 6
-        return x
 
     def trace(self):
         """
@@ -67,8 +91,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
-            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -79,34 +101,37 @@ class CPU:
 
         print()
 
-    def ram_read(self, address):
-        return self.reg[address]
+    def ram_read(self, mar):
+        return self.ram[mar]
 
     def ram_write(self, address, value):
         self.reg[address] = value
 
-    def ldi(self):
-        address = self.ram[self.pc + 1]
-        value = self.ram[self.pc + 2]
-        self.ram_write(address, value)
-        count = self.get_arg_count(LDI)
-        self.pc += count + 1
+    def push(self, op1, op2):
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = self.reg[op1]
+        return (2, True)
 
-    def prn(self):
-        address = self.ram[self.pc + 1]
-        print(self.ram_read(address))
-        count = self.get_arg_count(PRN)
-        self.pc += count + 1
-
-    def mul(self):
-        self.alu('MUL', self.ram[self.pc + 1], self.ram[self.pc + 2])
-        self.pc += self.get_arg_count(MUL) + 1
-
-    def hlt(self):
-        exit(0)
+    def pop(self, op1, op2):
+        self.reg[op1] = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
+        return (2, True)
 
     def run(self):
         """Run the CPU."""
-        run = True
-        while run:
-            self.instruction[self.ram[self.pc]]()
+        running = True
+
+        while running:
+            instruction = self.ram[self.pc]
+
+            op1 = self.ram_read(self.pc + 1)
+            op2 = self.ram_read(self.pc + 2)
+        
+            try:
+                opo = self.instruction[instruction](op1, op2)
+                running = opo[1]
+                self.pc += opo[0]
+
+            except:
+                print("Unknown instruction lol")
+                sys.exit(1)
